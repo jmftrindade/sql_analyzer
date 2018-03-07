bab7af4af63fc516fca78060df8ac76486ebbcd2
Joana Trindade <jmftrindade@gmail.com>
2018-03-06 19:29:26 -0500

    typo

--- src/analyzer/analyzer.py
+++ src/analyzer/analyzer.py
@@ -36,22 +36,22 @@ def printStats(json_filename):
 
       # Group by clauses.
       groupByColumns = jmespath.search(
-        'ast.statement[*].group.expression[*].name', entry)
+          'ast.statement[*].group.expression[*].name[]', entry)
       print 'groupBy columns: %s' % groupByColumns
 
       # Base tables when the query has no joins.
       baseTables = jmespath.search(
-        'ast.statement[?from.variant == \'table\'].from.name[]', entry)
+          'ast.statement[?from.variant == \'table\'].from.name[]', entry)
       print 'baseTables: %s' % baseTables
 
       # Base tables when the query has joins.
       baseTables += jmespath.search(
-        'ast.statement[?from.variant == \'join\'].from.source.name[]', entry)
+          'ast.statement[?from.variant == \'join\'].from.source.name[]', entry)
       print 'baseTables (with joins): %s' % baseTables
 
       # Join tables.
       joinTables = jmespath.search(
-        'ast.statement[?from.variant == \'join\'].from.map[*].source.name[]', entry)
+          'ast.statement[?from.variant == \'join\'].from.map[*].source.name[]', entry)
       print 'joinTables: %s' % joinTables
 
       # All tables mentioned in the query
@@ -70,8 +70,8 @@ def printStats(json_filename):
       # Index it into an LSH forest for top-k textually similar queries.
       queryLSH = getQueryMinHash(queryText)
       queryIndex[queryNum] = {
-        'queryText': queryText,
-        'queryLSH': queryLSH
+          'queryText': queryText,
+          'queryLSH': queryLSH
       }
       queriesLSHIndex.add(queryNum, queryLSH)
 
@@ -82,11 +82,11 @@ def printStats(json_filename):
       # queryNum,numExplicitJoins,referencedTables,groupByColumns,numGroupByColumns
       print 'queryNum = %s' % queryNum
       print 'csv:"%s","%s","%s","%s","%s"' % (
-        queryNum,
-        len(joinsLeft[0]) if len(joinsLeft) > 0 else 0,
-        ','.join(referencedTables),
-        ','.join(groupByColumns),
-        len(groupByColumns))
+          queryNum,
+          len(joinsLeft[0]) if len(joinsLeft) > 0 else 0,
+          ','.join(referencedTables),
+          ','.join(groupByColumns),
+          len(groupByColumns))
 
       # Populate a reverse index from table to script.
       tableToQuery = {}
@@ -109,10 +109,10 @@ def printStats(json_filename):
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(
-    description='Prints stats on SQL queries.')
+      description='Prints stats on SQL queries.')
   parser.add_argument('-i', '--input_json_filename',
-            help='Relative path of input JSON file containing SQL ASTs.',
-            required=True)
+                      help='Relative path of input JSON file containing SQL ASTs.',
+                      required=True)
   args = parser.parse_args()
 
   printStats(args.input_json_filename)
